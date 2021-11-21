327d0970b15356f844d7971275bbb71958ee83a4
k_takeda <amasok32@gmail.com>
2021-11-21 14:56:41 +0900

    CORS対策のミドルウェア実装 (#1)
    * move and rename server.go
    
    * gollira/muxの導入
    
    * cors対策のミドルウェア実装

--- cmd/graphql/main.go
+++ cmd/graphql/main.go
@@ -7,8 +7,10 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/amasok/sample-graphql/app/middleware"
 	"github.com/amasok/sample-graphql/app/presentation/graphql"
 	"github.com/amasok/sample-graphql/app/presentation/graphql/generated"
+	"github.com/gorilla/mux"
 )
 
 const defaultPort = "8080"
@@ -19,11 +21,13 @@ func main() {
 		port = defaultPort
 	}
 
+	r := mux.NewRouter()
+	r.Use(middleware.CORSForGraphql)
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graphql.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	r.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
