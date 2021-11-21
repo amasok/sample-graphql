4c9226180c3c7a36bdcf45adfcf3af98f3a73d58
amasok <amasok32@gmail.com>
2021-11-21 09:20:52 +0900

    gollira/muxの導入

--- cmd/graphql/main.go
+++ cmd/graphql/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/amasok/sample-graphql/app/presentation/graphql"
 	"github.com/amasok/sample-graphql/app/presentation/graphql/generated"
+	"github.com/gorilla/mux"
 )
 
 const defaultPort = "8080"
@@ -19,11 +20,13 @@ func main() {
 		port = defaultPort
 	}
 
+	r := mux.NewRouter()
+
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graphql.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	r.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
